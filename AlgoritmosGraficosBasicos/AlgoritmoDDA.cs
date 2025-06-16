0e49aef80a01073de2828b55e59ca12ee83deebc
Tenkenoz <epobando1@espe.edu.ec>
2025-06-16 16:39:44 -0500

    Se arreglo el algoritmo DDA

--- AlgoritmosGraficosBasicos/AlgoritmoDDA.cs
+++ AlgoritmosGraficosBasicos/AlgoritmoDDA.cs
@@ -10,39 +10,33 @@ namespace AlgoritmosGraficosBasicos
 {
     internal class AlgoritmoDDA : DibujarLinea
     {
-
         public override async void CalcularPuntos(PictureBox picCanvas)
         {
-           int deltaX = x2 - x1;
-           int deltaY = y2 - y1;
-           float auxX = x1;
-           float auxY= y1;
-           float m=(float)deltaY / deltaX;    
-
-                if (m < 0)
-                {
-                    for (int i = 0; i < Math.Abs(deltaX); i++)
-                    {
-                        GraficarPixel(picCanvas, (int)Math.Round(auxX), (int)Math.Round(auxY));
-                        EscribirCoordenadas((int)Math.Round(auxX), (int)Math.Round(auxY));
-                        auxX += 1;
-                        auxY += m;
-                    await Task.Delay(1); 
-                }
-                }
-                else
-                {
-                    for (int i = 0; i < Math.Abs(deltaY); i++)
-                    {
-                        GraficarPixel(picCanvas, (int)Math.Round(auxX), (int)Math.Round(auxY));
-                    EscribirCoordenadas((int)Math.Round(auxX), (int)Math.Round(auxY));
-                    auxX += 1 / m;
-                    auxY += 1;
-                    await Task.Delay(1); 
-                }
-            }
+            int deltaX = x2 - x1;
+            int deltaY = y2 - y1;
+            int pasos = Math.Max(Math.Abs(deltaX), Math.Abs(deltaY));
+            if (pasos == 0)
+            {
+                GraficarPixel(picCanvas, x1, y1);
+                EscribirCoordenadas(x1, y1);
                 MostrarCoordenadas(coordenadas);
-        }
+                return;
+            }
 
+            float incX = deltaX / (float)pasos;
+            float incY = deltaY / (float)pasos;
+            float auxX = x1;
+            float auxY = y1;
+
+            for (int i = 0; i <= pasos; i++)
+            {
+                GraficarPixel(picCanvas, (int)Math.Round(auxX), (int)Math.Round(auxY));
+                EscribirCoordenadas((int)Math.Round(auxX), (int)Math.Round(auxY));
+                auxX += incX;
+                auxY += incY;
+                await Task.Delay(1);
+            }
+            MostrarCoordenadas(coordenadas);
+        }
     }
 }
